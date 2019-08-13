fa6103a35e63214b88ed7fc90a9d5742738221c8
Robert <rbmanez@gmail.com>
2019-08-13 14:05:57 -0600

    delete techproject folder

--- techproject/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\robert\documents\tta\python\django_lessons\techproject\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
